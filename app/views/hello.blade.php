6eb7eed93fb1b3b4768b4edd9d3b72234b14a180
Fractaliste <raphael.nourrit@gmail.com>
2014-03-06 12:13:04 +0100

    Update the view to create the bug

---
+++ app/views/hello.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	{{stylesheet_link_tag()}}
+</head>
+<body>
+	<div class="welcome">
+{{image_tag('alm.png')}}		
+		<h1 id="myFont">You have arrived.</h1>
+	</div>
+</body>
+</html>
